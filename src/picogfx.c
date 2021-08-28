6c6a87f50dfc864681226bdc9eec92d68b904549
Johan Fransson <per.johan.fransson@gmail.com>
2021-08-28 20:59:51 +0200

    Revert "byte approach with framebuffer"
    This reverts commit 48be0b45ba04fbc0bd2aea09c03cb4f9c52b3ed4.

--- src/picogfx.c
+++ src/picogfx.c
@@ -19,7 +19,6 @@ typedef struct {
     Timing h;
     Timing v;
     float pixel_clock;
-    Uint8_t pixel_allowed_value;
 } HVTiming;
 
 void set_800_600(HVTiming *vga_timing, int scale) {
@@ -33,7 +32,6 @@ void set_800_600(HVTiming *vga_timing, int scale) {
     vga_timing->v.sync_pulse = 4;
     vga_timing->v.back_porch = 23;
     vga_timing->v.positive_pulse = 1;
-    vga_timing->v.pixel_allowed_value = 0;
     vga_timing->pixel_clock = 40000000.0/(float)scale;
 }
 
@@ -48,39 +46,65 @@ void set_640_480(HVTiming *vga_timing, int scale) {
     vga_timing->v.sync_pulse = 2;
     vga_timing->v.back_porch = 33;
     vga_timing->v.positive_pulse = 0;
-    vga_timing->v.pixel_allowed_value = 192;
     vga_timing->pixel_clock = 25000000.0/(float)scale;
+}
+
+
+void set_640_400(HVTiming *vga_timing, int scale) {
+    vga_timing->h.visible_area = 640/scale;
+    vga_timing->h.front_porch = 16/scale;
+    vga_timing->h.sync_pulse = 96/scale;
+    vga_timing->h.back_porch = 48/scale;
+    vga_timing->h.positive_pulse = 0;
+    vga_timing->v.visible_area = 400;
+    vga_timing->v.front_porch = 12;
+    vga_timing->v.sync_pulse = 2;
+    vga_timing->v.back_porch = 35;
+    vga_timing->v.positive_pulse = 1;
+    vga_timing->pixel_clock = 25175000.0/(float)scale;
 }
 
 uint16_t get_length(Timing *timing) {
     return timing->visible_area + timing->front_porch + timing->sync_pulse + timing->back_porch;
 }
 
-void init_row(uint32_t row[], Timing *t, uint8_t vsync_mask) {
-    uint8_t hsync_on_mask = 1 << HSYNC_BIT;
-    uint8_t hsync_off_mask = 0;
+void init_row(uint32_t row[], Timing *t, uint8_t pixel_mask, uint8_t vsync_mask) {
+    uint16_t pos = 0;
+    uint8_t rgb = 0;
+    uint32_t v = 0;
+    uint32_t vsync_mask32 = vsync_mask | (vsync_mask << 8) | (vsync_mask << 16) | (vsync_mask << 24);
+
+    uint8_t hsync_mask = 1 << HSYNC_BIT;
+    uint32_t hsync_off_mask32 = 0;
+    uint32_t hsync_on_mask32 = hsync_mask | (hsync_mask << 8) | (hsync_mask << 16) | (hsync_mask << 24);
     if (!t->positive_pulse) {
-        hsync_off_mask = hsync_on_mask;
-        hsync_on_mask = 0;
+        hsync_off_mask32 = hsync_on_mask32;
+        hsync_on_mask32 = 0;
     }
 
-    uint16_t pos = 0;
     for (int i = 0; i < t->visible_area; i++) {
-        row[pos++] = hsync_off_mask | vsync_mask;
+        v = v >> 8;
+        v |= (rgb & pixel_mask) << 24;
+        if (i % 4 == 3) {
+            row[pos++] = v | hsync_off_mask32 | vsync_mask32;
+            v = 0;
         }
+        rgb++;
     }
-    for (int i = 0; i < t->front_porch; i++) {
-        row[pos++] = hsync_off_mask | vsync_mask;
+    for (int i = 0; i < t->front_porch/4; i++) {
+        row[pos++] = hsync_off_mask32 | vsync_mask32;
     }
-    for (int i = 0; i < t->sync_pulse; i++) {
-        row[pos++] = hsync_on_mask | vsync_mask;
+    for (int i = 0; i < t->sync_pulse/4; i++) {
+        row[pos++] = hsync_on_mask32 | vsync_mask32;
     }
-    for (int i = 0; i < t->back_porch; i++) {
-        row[pos++] = hsync_off_mask | vsync_mask;
+    for (int i = 0; i < t->back_porch/4; i++) {
+        row[pos++] = hsync_off_mask32 | vsync_mask32;
     }
 }
 
 int main() {
+//    set_sys_clock_khz(140000, true);
+//    clock_configure(clk_sys, 0, 0, 0, 140000000);
     const int scale = 2;
 
     HVTiming vga_timing;
@@ -92,7 +116,7 @@ int main() {
     const uint8_t visibleRow = 0;
     const uint8_t invisibleRow = 1;
     const uint8_t vsyncRow = 2;
-    uint8_t row[3][columns];
+    uint32_t row[3][columns/4];
     uint8_t vsync_on_mask = 1 << VSYNC_BIT;
     uint8_t vsync_off_mask = 0;
     if (!vga_timing.v.positive_pulse) {
@@ -129,23 +153,12 @@ int main() {
     //float div = 10000;
     vga_program_init(pio, sm, offset, VGA_BASE_PIN, div);
 
-    uint32_t framebuffer_size = 400*300;
-    uint8_t framebuffer[framebuffer_size];
-    for (int i = 0; i < framebuffer_size; i++) {
-        framebuffer[i] = i & 63;
-    }
-
-    uint32_t v = 0;
     while (true) {
         for (uint16_t y = 0 ; y < rows; y++) {
             uint8_t row_type = row_def[y];
-            uint8_t *sync = row[row_type];
-            for (uint16_t x = 0; x < columns; x++) {
-                v = sync[x] | (sync[x+1] << 8) | (sync[x+2] << 16) | (sync[x+3] << 24);
-                if (sync[x] == vga_timing.pixel_allowed_value) {
-                    v|= framebuffer[x] | (framebuffer[x+1] << 8) | (framebuffer[x+2] << 16) | (framebuffer[x+3] << 24);
-                }
-                pio_sm_put_blocking(pio, sm, v);
+            uint32_t *scanline = row[row_type];
+            for (uint16_t x = 0; x < columns/4; x++) {
+                pio_sm_put_blocking(pio, sm, scanline[x]);
             }
         }
     }
